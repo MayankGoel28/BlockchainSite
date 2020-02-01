db4e951d7077d91eddbe31f7912150cf025adf40
Mayank Goel <themayankgoel28@gmail.com>
2020-02-02 00:13:13 +0530

    new repo

---
+++ precache-manifest.3a6c32d90600995d4018d0f014a6d315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d1544356dbd2bcaae1cf30c3961d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fd322671ad948590883",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "00d54e689c2a5b0cd73e",
+    "url": "/static/css/main.eb882395.chunk.css"
+  },
+  {
+    "revision": "1fd322671ad948590883",
+    "url": "/static/js/2.76bd987a.chunk.js"
+  },
+  {
+    "revision": "00d54e689c2a5b0cd73e",
+    "url": "/static/js/main.7b12ddd9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
